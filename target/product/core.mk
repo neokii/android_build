376ba6a0e46eeb543cc9ebd155d640ddf03112dd
neokii <neokii@gmail.com>
2020-07-11 18:03:02 +0900

    Update core.mk

--- target/product/core.mk
+++ target/product/core.mk
@@ -57,6 +57,7 @@ PRODUCT_PACKAGES += \
     Telecom \
     TeleService \
     VpnDialogs \
-    MmsService
+    MmsService \
+    NEOSSetup
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
